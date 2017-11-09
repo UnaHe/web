e98a88b1ae1529c1269d707eebbf375eed75661c
david4liu <david4liu@gmail.com>
2017-11-09 11:59:45 +0800

    测试淘口令

--- app/Services/TransferService.php
+++ app/Services/TransferService.php
@@ -371,11 +371,11 @@ class TransferService
             }
             $lastUrl =  "http://item.taobao.com/item.htm?id=".$matchItemId[1];
         }
-        var_dump($lastUrl);exit;
+        //var_dump($lastUrl);exit;
         if(!strpos($lastUrl, "uland.taobao.com")){
             if(preg_match("/item\.taobao\.com.*?[\?&]id=(\d+)/", $lastUrl, $matchItemId)){
                 $matchItemId = $matchItemId[1];
-            }else if(preg_match("/edtail\.tmall\.com.*?[\?&]id=(\d+)/", $lastUrl, $matchItemId)){
+            }else if(preg_match("/edetail\.tmall\.com.*?[\?&]id=(\d+)/", $lastUrl, $matchItemId)){
                 $matchItemId = $matchItemId[1];
             }
 
