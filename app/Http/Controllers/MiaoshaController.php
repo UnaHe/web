d86d40276505c007316c4fd0db88b847ab5ef283
formatcc <young123tao@163.com>
2017-11-30 17:02:03 +0800

    秒杀商品列表图片格式

--- app/Http/Controllers/MiaoshaController.php
+++ app/Http/Controllers/MiaoshaController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Helpers\CacheHelper;
+use App\Helpers\GoodsHelper;
 use App\Services\ChannelColumnService;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
@@ -37,6 +38,9 @@ class MiaoshaController extends Controller
         $params = $request->all();
         if(!$data = CacheHelper::getCache($params)){
             $data = (new ChannelColumnService())->miaoshaGoods($activeTime);
+            if($data){
+                $data = (new GoodsHelper())->resizeGoodsListPic($data->toArray(), ['pic'=>'240x240']);
+            }
             CacheHelper::setCache($data, 5, $params);
         }
 
