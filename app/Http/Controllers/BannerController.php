f81ffe8239e016fab3f33e1dcbe57633640ef51b
unah <by2live@hotmail.com>
2017-12-06 21:31:32 +0800

    1212Banner

--- app/Http/Controllers/BannerController.php
+++ app/Http/Controllers/BannerController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 use App\Services\BannerService;
+use Illuminate\Http\Request;
+use App\Services\TaobaoService;
 
 /**
  * banner广告
@@ -13,8 +15,16 @@ class BannerController extends Controller
     /**
      * 获取指定位置广告
      */
-    public function getBanner($position){
+    public function getBanner(Request $request, $position){
         $data = (new BannerService())->getBanner($position);
+        foreach ($data as $k=>$v) {
+            if (!empty($v['click_url'])) {
+                $userId = $request->user()->id;
+                $pid = (new TaobaoService())->getPid($userId);
+                $data[$k]['click_url'] = str_replace('@userId@', $userId, $data[$k]['click_url']);
+                $data[$k]['click_url'] = str_replace('@pid@', $pid, $data[$k]['click_url']);
+            }
+        }
         return $this->ajaxSuccess($data);
     }
 
