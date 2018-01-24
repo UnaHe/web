9f8b401415a7275bfc33a430747b608bf5ea3dcb
dll <donglilai@tuike198.com>
2018-01-24 10:46:09 +0800

    路径修改

--- resources/views/web/layouts/entrance.blade.php
+++ resources/views/web/layouts/entrance.blade.php
@@ -14,7 +14,7 @@
     <meta name="renderer" content="webkit">
     <meta name="csrf-token" content="{{csrf_token()}}"/>
     <meta http-equiv="Cache-Control" content="no-siteapp"/>
-    <link rel="stylesheet" href="assets/css/amazeui.css"/>
+    <link rel="stylesheet" href="/assets/css/amazeui.css"/>
     <link href="/css/web/common.css" rel="stylesheet" type="text/css">
     <link href="/css/web/user/dlstyle.css" rel="stylesheet" type="text/css">
     <link rel="stylesheet" href="/web/lib/bootstrapvalidator/dist/css/bootstrapValidator.min.css">
