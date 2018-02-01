bcc5d5051fc6da6fbe9b1985a05b84fba54126cd
una <by2live@hotmail.com>
2018-02-01 11:51:04 +0800

    分享app对应标题

--- app/Http/Controllers/ShareController.php
+++ app/Http/Controllers/ShareController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Services\UserService;
 use Illuminate\Http\Request;
 use App\Helpers\UrlHelper;
+use App\Models\shareTitle;
 
 /**
  * 分享链接
@@ -17,14 +18,20 @@ class ShareController extends Controller
      * 朋友淘分享链接
      * @param Request $request
      * @return static
+     * @throws \Exception
      */
     public function getShare(Request $request)
     {
+        $appName = $request->input('from');
+
+        // 分享标题.
+        $title = shareTitle::where('app_name', $appName)->pluck('title')->first();
+
         // 获取当前用户推荐码.
         $code = (new UserService())->getReferralCode($request->user());
 
         // 拼接邀请链接.
-        $longUrl = 'http://'.config('domains.pytao_domains').'/?u='.$code."&t=".time();
+        $longUrl = 'http://'.config('domains.pytao_domains').'/?u='.$code.'&t='.time().'&title='.$title;
 
         // 短链接.
         $shortUrl = (new UrlHelper())->shortUrl($longUrl);
