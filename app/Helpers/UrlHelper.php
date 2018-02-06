f2df24b75c62010aae007a71aaeb19f2bec00786
formatcc <young123tao@163.com>
2018-02-06 12:58:36 +0800

    新浪短链接转链失败问题修复

--- app/Helpers/UrlHelper.php
+++ app/Helpers/UrlHelper.php
@@ -21,7 +21,8 @@ class UrlHelper
     public function shortUrl($url){
         $url = urlencode($url);
         $appId = config('services.sina_open.key');
-        $apiUrl = "http://api.weibo.com/2/short_url/shorten.json?source=5786724301&url_long=".$url;
+//        $apiUrl = "http://api.weibo.com/2/short_url/shorten.json?source=5786724301&url_long=".$url;
+        $apiUrl = "http://api.t.sina.com.cn/short_url/shorten.json?source={$appId}&url_long=".$url;
         $client = new Client(['timeout' => 3]);
         try{
             if((new SysConfigService())->get('sina_short_url') != 1){
@@ -32,16 +33,16 @@ class UrlHelper
                 throw new \Exception('短网址转换失败');
             }
             $response = json_decode($response, true);
-            $code = parse_url($response['urls'][0]['url_short']);
+            $code = parse_url($response[0]['url_short']);
             $path = trim($code['path'], '/');
-            if(!isset($response['urls'][0]['url_short'])){
+            if(!isset($response[0]['url_short'])){
                 return null;
             }
             // 转换格式不正确进入下一次转换.
             if (!(strlen($path) > 1 && strlen($path) <= 8)){
                 throw new \Exception('短网址转换失败');
             }
-            return $response['urls'][0]['url_short'];
+            return $response[0]['url_short'];
         }catch (\Exception $e){
             $apiUrl = "http://xapi.in/urls/add?&secretkey=00007821518940188wzk.im!@6b2494e&lurl=".$url;
             $response = $client->get($apiUrl)->getBody()->getContents();
