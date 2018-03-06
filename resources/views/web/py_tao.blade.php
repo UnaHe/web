8664f6c48e81b92541e25195912863b99b998a2e
una <by2live@hotmail.com>
2018-03-06 20:30:00 +0800

    完成朋友淘单页

--- resources/views/web/py_tao.blade.php
+++ resources/views/web/py_tao.blade.php
@@ -17,7 +17,7 @@
     <header class="herder">
         <div class="container">
             <div class="herder-left">
-                <a href="/"><img alt="logo" src="/images/web/py_tao/LOGO.png"/><span class="herder-left-title">朋友淘</span></a>
+                <a href="/"><img alt="logo" src="/images/web/py_tao/py_tao_logo.png"/><span class="herder-left-title">朋友淘</span></a>
             </div>
             <ul class="herder-nav">
                 <li><a>主页</a></li>
@@ -36,54 +36,136 @@
                 <p class="font_size28">朋友淘</p>
                 <p class="font_size28">汇聚全网优惠券</p>
                 <p class="font_size28">让你不仅省钱，还赚钱</p>
-                <img src="/images/web/py_tao/qr_code.png" />
+                <img src="/images/web/py_tao/py_tao_qr_code.png" />
                 <p class="font_size28">每天都是双十一</p>
                 <p class="font_size28">扫码关注官方微信公众号下载</p>
-                <div class="open_btn">立即下载</div>
+                <a href=""><div class="open_btn">立即下载</div></a>
             </div>
             <div class="pytao-right">
                 <img src="/images/web/py_tao/py_tao.png" />
             </div>
         </div>
-        <!--联系我们-->
-        <div class="contact_us">
-            <div class="contact_us_title">
-                <p class="font_size28">联系我们</p>
-            </div>
-            <div id="contact_us_box_all">
-                <div class="contact_us_box">
-                    <div class="circle">
-                        <img src="/images/web/about_us_phone.png" alt="" />
-                    </div>
-                    <p class="font_size16">给我们打电话</p>
-                    <p class="font_size28">028-83173171</p>
+        <!--历史-->
+        <div class="history">
+            <div id="history_box_all">
+                <div class="history_box history_bg_color1">
+                    <img class="history_box_img" src="/images/web/py_tao/py_tao_icon1.png" alt="" />
+                    <p class="font_size24 history_box_title">朋友淘APP的诞生</p>
+                    <p class="font_size16 history_box_content">2017年3月项目策划团队筹建，2017年5月测试版APP上线，2017年8月正</p>
+                    <p class="font_size16 history_box_content">式版上线，2018年2月1日。我们上线2.0全新版本。我们不断优化用户的购</p>
+                    <p class="font_size16 history_box_content">物体验，为大家创造额外的被动可持续收入。躺赚，不仅仅是一个名词！而</p>
+                    <p class="font_size16 history_box_content">是看得见的被动收益。</p>
                 </div>
-                <div class="contact_us_box">
-                    <div class="circle">
-                        <img src="/images/web/about_us_QQ.png" alt="" />
-                    </div>
-                    <p class="font_size16">QQ在线联系</p>
-                    <p class="font_size28">1280977784</p>
+                <div class="history_box history_bg_color2">
+                    <img class="history_box_img" src="/images/web/py_tao/py_tao_icon2.png" alt="" />
+                    <p class="font_size24 history_box_title">朋友淘的优势</p>
+                    <p class="font_size16 history_box_content">轻松赚钱 轻松创业，0库存！0风险！低门槛！永久受益！整合数万的电商</p>
+                    <p class="font_size16 history_box_content">商家，每天多达几万的品类商品。供你分享赚钱，我们同步聚划算等多个模</p>
+                    <p class="font_size16 history_box_content">块。超级爆款限时抢购，上新品质生活极有家，实物实拍更真实的视频购，</p>
+                    <p class="font_size16 history_box_content">还有贴心的9.9包邮。品牌闪购，大牌旗舰店等你来逛。排序方式多样化，</p>
+                    <p class="font_size16 history_box_content">券后价，优惠金额，奖励金额，销量等。更多惊喜等你体验！</p>
                 </div>
-                <div class="contact_us_box">
-                    <div class="circle">
-                        <img src="/images/web/about_us_email.png" alt="" />
-                    </div>
-                    <p class="font_size16">给我们发邮件</p>
-                    <p class="font_size28">service@tuike198.com</p>
+                <div class="history_box history_bg_color3">
+                    <img class="history_box_img" src="/images/web/py_tao/py_tao_icon3.png" alt="" />
+                    <p class="font_size24 history_box_title">朋友淘的未来</p>
+                    <p class="font_size16 history_box_content">朋友淘数万运营商和数十万商家与你一起创造美好的明天，欢迎宝妈、微商</p>
+                    <p class="font_size16 history_box_content">、线上线下商家、淘客加入我们的大家庭！</p>
                 </div>
             </div>
         </div>
-        <!--关注微信-->
-        <div class="follow_wechat">
-            <div class="follow_wechat_title">
-                <p class="font_size28">关注微信</p>
+        <!--注册流程-->
+        <div class="reg">
+            <div class="reg-left">
+                <p class="font_size24 reg-left-title">朋友淘注册流程</p>
+                <p class="font_size16 reg-left-des">朋友淘通过邀请注册，扫码确定关系终身不变，比亲妈还亲。</p>
+                <p class="font_size16 reg-left-content"><span>1</span>点击网页下载链接，下载注册朋友淘，填写邀请码4jdlka</p>
+                <p class="font_size16 reg-left-content"><span>2</span>登陆后.扫码添加微信客服，或者加alibabashuyao.回复手机后四位赠送代理</p>
+                <a href=""><div class="open_btn">立即下载</div></a>
             </div>
-            <div class="follow_wechat_content">
-                <p class="font_size16">扫一扫二维码，关注我们的微信公众号</p>
+            <div class="reg-right">
+                <img src="/images/web/py_tao/py_tao_reg.png" />
             </div>
-            <div class="follow_wechat_qr">
-                <img src="/images/web/about_us_wechat_qr.png" alt="" />
+        </div>
+        <!--分享赚钱-->
+        <div class="share">
+            <div class="share-box">
+                <div class="share-flex share-left">
+                    <img src="/images/web/py_tao/share.png" />
+                </div>
+                <div class="share-flex share-right">
+                    <p class="font_size24 share-left-title">如何分享赚钱</p>
+                    <p class="font_size16 share-left-des">在APP端，右下角我-分享好友中，我们有多重推广方式，</p>
+                    <p class="font_size16 share-left-des">以推广APP为主，用你的邀请码或者邀请链接下载注册的</p>
+                    <p class="font_size16 share-left-des margin-bottom">自动成为你团队小伙伴。当然，你也可以分享商品口令推荐你所喜欢的商品</p>
+                    <p class="font_size16 share-left-content"><span>1</span>APP邀请推广</p>
+                    <p class="font_size16 share-left-content"><span>2</span>商品分享推广</p>
+                    <p class="font_size16 share-left-content"><span>3</span>多种方式推广</p>
+                    <a href=""><div class="open_btn">立即下载</div></a>
+                </div>
+            </div>
+        </div>
+        <!--角色划分-->
+        <div class="character">
+            <div class="character-title">
+                <p class="font_size24">朋友淘角色划分</p>
+            </div>
+            <div class="character-content">
+                <p class="font_size16">小伙伴，咱们有朋友，有达人，有店主哦。想加入运营商的宝宝们欢迎私聊我们的美女客服^.^</p>
+            </div>
+            <div id="character-box-all">
+                <ul class="character-box">
+                    <li class="character-box-title">
+                        <p class="font_size24 character-box-title1 bold colorBlack">朋友</p>
+                        <p class="font_size16 character-box-subtitle">Friends</p>
+                    </li>
+                    <li class="character-box-content font_size16"><span class="disc"></span>免费使用</li>
+                    <li class="character-box-content font_size16 border-top"><span class="disc"></span>领取大额优惠券</li>
+                    <li class="character-box-content font_size16 border-top"><span class="disc"></span>可分享好友</li>
+                    <li class="character-box-content font_size16 border-top box-shadow"><span class="disc"></span>自买省钱</li>
+                </ul>
+                <ul class="character-box">
+                    <li class="character-box-title">
+                        <p class="font_size24 character-box-title1 bold colorOrange">达人</p>
+                        <p class="font_size16 character-box-subtitle">Talent</p>
+                    </li>
+                    <li class="character-box-content font_size16"><span class="disc"></span>邀请10个新用户可自动升级</li>
+                    <li class="character-box-content font_size16 border-top"><span class="disc"></span>邀请码加客服可直接赠送</li>
+                    <li class="character-box-content font_size16 border-top"><span class="disc"></span>消费有佣金</li>
+                    <li class="character-box-content font_size16 border-top box-shadow"><span class="disc"></span>自买省上省，分享赚不停</li>
+                </ul>
+                <ul class="character-box">
+                    <li class="character-box-title">
+                        <p class="font_size24 character-box-title1 bold colorPurple">店长</p>
+                        <p class="font_size16 character-box-subtitle">Shopkeeper</p>
+                    </li>
+                    <li class="character-box-content font_size16"><span class="disc"></span>年付Or月付+收点</li>
+                    <li class="character-box-content font_size16 border-top"><span class="disc"></span>必须满足一定条件</li>
+                    <li class="character-box-content font_size16 border-top"><span class="disc"></span>可以免费开通代理商</li>
+                    <li class="character-box-content font_size16 border-top box-shadow"><span class="disc"></span>具体咨询客服</li>
+                </ul>
+            </div>
+        </div>
+        <!--赶快下载-->
+        <div class="download">
+            <div class="download-box">
+                <div class="download-left-pic">
+                    <img src="/images/web/py_tao/py_tao_lift.png" />
+                </div>
+                <div class="download-cen">
+                    <div class="download-title">
+                        <p class="font_size24">赶快下载使用吧！</p>
+                    </div>
+                    <div class="download-content">
+                        <p class="font_size16">高速下载通请点击下方下载</p>
+                    </div>
+                    <div class="download-btn">
+                        <a href=""><div class="open_btn download-btn1">安卓·Android</div></a>
+                        <a href=""><div class="open_btn download-btn2">苹果·App Store</div></a>
+                    </div>
+                </div>
+                <div class="download-right-pic">
+                    <img src="/images/web/py_tao/py_tao_right.png" />
+                </div>
             </div>
         </div>
     </seation>
